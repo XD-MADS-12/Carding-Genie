86e15b643ad18e9eb7591564da97138f788fc23a
XD ATIK VAU <145763843+XD-MADS-12@users.noreply.github.com>
2026-01-08 13:10:39 +0600

    Create supabaseClient.js

---
+++ src/utils/supabaseClient.js
@@ -0,0 +1,10 @@
+// src/utils/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+// Initialize Supabase client
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export { supabase };
