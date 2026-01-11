b4ad0b2294a33448893afba0ef8c404061b01f93
XD ATIK VAU <145763843+XD-MADS-12@users.noreply.github.com>
2026-01-11 10:23:55 +0600

    Create supabaseClient.js

---
+++ src/services/supabaseClient.js
@@ -0,0 +1,13 @@
+// src/services/supabaseClient.js
+import { createClient } from '@supabase/supabase-js';
+
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
+const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
+
+if (!supabaseUrl || !supabaseKey) {
+  console.error('Supabase URL or Key is not set in environment variables');
+}
+
+const supabase = createClient(supabaseUrl, supabaseKey);
+
+export default supabase;
