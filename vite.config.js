3e115622fa03c5941418cdbc45ea8e70608e420d
XD ATIK VAU <145763843+XD-MADS-12@users.noreply.github.com>
2026-01-11 10:31:01 +0600

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -5,7 +5,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    global: 'globalThis',
-  }
+  server: {
+    host: true,
+    port: 3000,
+  },
+  build: {
+    outDir: 'dist',
+    sourcemap: true,
+  },
+  optimizeDeps: {
+    exclude: ['lucide-react'],
+  },
 })
